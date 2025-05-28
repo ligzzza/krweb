b8952d577c8ec5abc077885ff52e816736fe7539
ligzzza <galova.liza06@gmail.com>
2025-05-28 11:51:57 +0300

    4 задание (html)

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,14 @@
-from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Привет, мир! Это мой первый Django-сайт!")
+
+from django.shortcuts import render
+from .models import geeхам  # ваша модель
+
+def exam(request):
+    # Получаем все опубликованные записи модели geeхам
+    exams = geeхам.objects.filter(is_public=True)
+    context = {
+        'exams': exams,
+        'fio': 'Ваше ФИО',              # замените на ваше ФИО
+        'group_number': 'Номер группы'  # замените на номер вашей группы
+    }
+    return render(request, 'geexam.html', context)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from myapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('exam/', include('myapp.urls')),
+    path('', views.exam, name='home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
